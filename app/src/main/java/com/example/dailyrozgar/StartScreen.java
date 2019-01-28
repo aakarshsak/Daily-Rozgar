42bde08b29fa9af2401884b0f81caa5c7206b632
Aakarsh Sinha <aakarshsak@gmail.com>
2019-01-28 06:39:59 +0530

    Adding recycler view for developers name but stuck with a problem

--- app/src/main/java/com/example/dailyrozgar/StartScreen.java
+++ app/src/main/java/com/example/dailyrozgar/StartScreen.java
@@ -3,11 +3,24 @@ package com.example.dailyrozgar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.dailyrozgar.Developers.DevCustomAdapter;
+import com.example.dailyrozgar.Developers.DevData;
+import com.example.dailyrozgar.Developers.DevDataModel;
+
+import java.util.ArrayList;
+
 public class StartScreen extends AppCompatActivity {
     ImageView userImage,workerImage;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    static ArrayList<DevDataModel> dataModels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +28,21 @@ public class StartScreen extends AppCompatActivity {
 
         userImage=findViewById(R.id.userImage);
         workerImage=findViewById(R.id.workerImage);
+        recyclerView=findViewById(R.id.startRecycler);
 
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        dataModels=new ArrayList<DevDataModel>();
+
+        for(int i=0;i<3;i++)
+        {
+            dataModels.add(new DevDataModel(DevData.devNameArray[i],DevData.devDescArray[i],DevData.devImageArray[i]));
+        }
+
+        adapter=new DevCustomAdapter(dataModels);
+        recyclerView.setAdapter(adapter);
 
         userImage.setOnClickListener(new View.OnClickListener() {
             @Override
